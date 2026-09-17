8dca3609468e873ac5cb737e6bea4457ee79535a
bidmybl <bidmyblood07@gmail.com>
2026-09-17 15:11:02 +0300

    implement basic work with json

--- main.go
+++ main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type User struct {
+	Name string `json:"name"`
+	Age  string `json:"age"`
+}
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!")
 }
 
+func user(w http.ResponseWriter, r *http.Request) {
+	u := User{
+		Name: r.URL.Query().Get("name"),
+		Age:  r.URL.Query().Get("age"),
+	}
+	if u.Name == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Name is required!")
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(u)
+}
+
 func main() {
 	http.HandleFunc("/", greet)
+	http.HandleFunc("/user", user)
 	http.ListenAndServe(":8080", nil)
 }
