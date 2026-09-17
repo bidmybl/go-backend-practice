6e48eef17bed481809fd5545741670c6163ae703
bidmybl <bidmyblood07@gmail.com>
2026-09-17 21:35:41 +0300

    Add GET user endpoint

--- main.go
+++ main.go
@@ -23,31 +23,34 @@ func greet(w http.ResponseWriter, r *http.Request) {
 var users []User
 
 func user(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
+	switch r.Method {
+	case http.MethodPost:
+		var user User
+		err := json.NewDecoder(r.Body).Decode(&user)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprint(w, "Invalid JSON")
+			return
+		}
+		if user.Name == "" || user.Age == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprint(w, "Name or Age(or both) is incorrect")
+			return
+		}
+		users = append(users, user)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		response := UserResponse{
+			Message: "User Created",
+			Data:    user,
+		}
+		json.NewEncoder(w).Encode(response)
+	case http.MethodGet:
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(users)
+	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
 	}
-
-	var user User
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, "Invalid JSON")
-		return
-	}
-	if user.Name == "" || user.Age == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, "Name or Age(or both) is incorrect")
-		return
-	}
-	users = append(users, user)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	response := UserResponse{
-		Message: "User Created",
-		Data:    user,
-	}
-	json.NewEncoder(w).Encode(response)
 }
 
 func main() {
